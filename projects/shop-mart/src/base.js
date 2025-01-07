ba48b454ae7e518505f672a66c6781c26984c5fb
Ankur Gupta <ankurgupta@web.de>
2025-01-07 20:40:00 +0100

    fix: removed hardcoded value

--- projects/shop-mart/src/base.js
+++ projects/shop-mart/src/base.js
@@ -4,13 +4,13 @@ import "firebase/firestore"
 import "firebase/auth"
 
 const config = {
-  apiKey: "AIzaSyCUY4DrZ-l6H2se8YuKU7t1Jm6Ox1RoG88",
+  apiKey: process.env.API_KEY,
   authDomain: process.env.SHOPMART_FIREBASE_DOMAIN,
   databaseURL: process.env.SHOPMART_FIREBASE_DATABASE,
   projectId: "shop-mart-df15a",
   storageBucket: process.env.SHOPMART_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.SHOPMART_FIREBASE_SENDER_ID,
-}
+};
 //firebase.firestore().settings({ timestampsInSnapshots: true })
 //const db = app.firestore().settings({ timestampsInSnapshots: true })
 //export { db }
